c89f59e7d72345aaab64dba3bbb0d790a2efc44a
Carlos Hernandez <cralitos@gmail.com>
2022-09-30 20:31:20 -0600

    se agregamn corchetes a la propiedad del boton y se vincula con el atributo de la clase ts

--- src/app/lista-personas/lista-personas.component.ts
+++ src/app/lista-personas/lista-personas.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaPersonasComponent implements OnInit {
 
+  agregarPersona=false;
   constructor() { }
 
   ngOnInit(): void {
